2f296987688e5adb5038c80b06ad70a396dae7e5
guilherme21alcantara <57152347+guilherme21alcantara@users.noreply.github.com>
2021-05-12 17:19:59 -0300

    wehoo

---
+++ src/@core/layouts/components/navbar/UserDropdown.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '@components/avatar'
+import { isUserLoggedIn } from '@utils'
+import { useDispatch } from 'react-redux'
+import { handleLogout } from '@store/actions/auth'
+import { UncontrolledDropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'reactstrap'
+import { User, Mail, CheckSquare, MessageSquare, Settings, CreditCard, HelpCircle, Power } from 'react-feather'
+import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg'
+
+
+const UserDropdown = () => {
+  const dispatch = useDispatch()
+  const [userData, setUserData] = useState(null)
+  useEffect(() => {
+    if (isUserLoggedIn() !== null) {
+      setUserData(JSON.parse(localStorage.getItem('userData')))
+    }
+  }, [])
+
+
+  const userAvatar = (userData && userData.avatar) || defaultAvatar
+
+  return (
+    <UncontrolledDropdown tag='li' className='dropdown-user nav-item'>
+      <DropdownToggle href='/' tag='a' className='nav-link dropdown-user-link' onClick={e => e.preventDefault()}>
+        <div className='user-nav d-sm-flex d-none'>
+          <span className='user-name font-weight-bold'>{(userData && userData['username']) || 'John Doe'}</span>
+          <span className='user-status'>{(userData && userData.role) || 'Admin'}</span>
+        </div>
+        <Avatar img={userAvatar} imgHeight='40' imgWidth='40' status='online' />
+      </DropdownToggle>
+      <DropdownMenu right>
+        <DropdownItem tag={Link} to='/pages/profile'>
+          <User size={14} className='mr-75' />
+          <span className='align-middle'>Perfil</span>
+        </DropdownItem>
+        <DropdownItem tag={Link} to='/apps/email'>
+          <Mail size={14} className='mr-75' />
+          <span className='align-middle'>Email</span>
+        </DropdownItem>
+        <DropdownItem tag={Link} to='/apps/chat'>
+          <MessageSquare size={14} className='mr-75' />
+          <span className='align-middle'>Chats</span>
+        </DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem tag={Link} to='/pages/account-settings'>
+          <Settings size={14} className='mr-75' />
+          <span className='align-middle'>Settings</span>
+        </DropdownItem>        
+        <DropdownItem tag={Link} to='/login' onClick={() => dispatch(handleLogout())}>
+          <Power size={14} className='mr-75' />
+          <span className='align-middle'>Sair</span>
+        </DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  )
+}
+
+export default UserDropdown
